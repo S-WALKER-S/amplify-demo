5016a4313d2a04e1ee0a6f4e39bacaa97032e472
WALKER <sasaarimo@gmail.com>
2022-01-21 22:19:24 +0900

    added auth

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
 import React from 'react';
-import Amplify, { Auth } from "aws-amplify";
-import awsmobile from "./aws-exports";
-import { withAuthenticator } from "aws-amplify-react";
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-// Amplifyの設定を行う
-Amplify.configure(awsmobile)
-
-// SingUp時に、メールアドレスとパスワードを要求する
-const signUpConfig = {
-    header: 'Sign Up',
-    hideAllDefaults: true,
-    defaultCountyCode: '1',
-    signUpFields: [
-        {
-            label: 'User Name',
-            key: 'username',
-            required: true,
-            displayOrder: 1,
-            type: 'string'
-        },
-        {
-            label: 'Email',
-            key: 'email',
-            required: true,
-            displayOrder: 2,
-            type: 'string'
-        },
-        {
-            label: 'Password',
-            key: 'password',
-            required: true,
-            displayOrder: 3,
-            type: 'password'
-        }
-    ]
-}
-
-// SingOut
-function signOut() {
-    Auth.signOut()
-        .then()
-        .catch();
-}
 
 function App() {
     return (
-        <React.Fragment>
-            <button onClick={signOut}>Sign out</button>
-            <div>
-                Hello World
-      </div>
-        </React.Fragment>
+        <div className="App">
+            <header>
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>We now have Auth!</h1>
+            </header>
+            <AmplifySignOut />
+        </div>
     );
 }
 
-// Appコンポーネントをラップする
-export default withAuthenticator(App, { signUpConfig });
+export default withAuthenticator(App);
