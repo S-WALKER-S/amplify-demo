7834590932490d650f59ed4c55f5edbf4fdc194d
WALKER <sasaarimo@gmail.com>
2022-01-21 21:29:44 +0900

    changes for v3

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "testappb917bd3d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify, { Auth } from "aws-amplify";
+import awsmobile from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
+
+// Amplifyの設定を行う
+Amplify.configure(awsmobile)
+
+// SingUp時に、メールアドレスとパスワードを要求する
+const signUpConfig = {
+    header: 'Sign Up',
+    hideAllDefaults: true,
+    defaultCountyCode: '1',
+    signUpFields: [
+        {
+            label: 'User Name',
+            key: 'username',
+            required: true,
+            displayOrder: 1,
+            type: 'string'
+        },
+        {
+            label: 'Email',
+            key: 'email',
+            required: true,
+            displayOrder: 2,
+            type: 'string'
+        },
+        {
+            label: 'Password',
+            key: 'password',
+            required: true,
+            displayOrder: 3,
+            type: 'password'
+        }
+    ]
+}
+
+// SingOut
+function signOut() {
+    Auth.signOut()
+        .then()
+        .catch();
+}
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Hello from V2</h1>
-            </header>
-        </div>
+        <React.Fragment>
+            <button onClick={signOut}>Sign out</button>
+            <div>
+                Hello World
+      </div>
+        </React.Fragment>
     );
 }
 
-export default App;
+// Appコンポーネントをラップする
+export default withAuthenticator(App, { signUpConfig });
